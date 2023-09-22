5d949d30e84aca0930255db5f91cec8157810e78
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-09-22 13:43:08 +0900

    feat: app icon

---
+++ README.md
@@ -0,0 +1,6 @@
+# lancher_icons_app
+
+iconを生成するコマンド
+```bash
+flutter pub run flutter_launcher_icons:main
+```
---
+++ android/app/src/main/kotlin/com/jboy/lancher_icons_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jboy.lancher_icons_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
